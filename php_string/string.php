4e240d8746c064c0cf942740c425c1e881941d10
Ishahahah <132723077+Ishahahah@users.noreply.github.com>
2023-11-29 00:38:47 +0530

    Create string.php

---
+++ php_string/string.php
@@ -0,0 +1,22 @@
+<?php
+function transformString($inputString, $option) {
+    switch($option) {
+        case 'UC':
+            return strtoupper($inputString); // Convert to uppercase
+        case 'LC':
+            return strtolower($inputString); // Convert to lowercase
+        case 'FCUC':
+            return ucfirst(strtolower($inputString)); // Convert first character to uppercase
+        default:
+            return "Invalid option.";
+    }
+}
+
+// Example usage:
+$string = "hello, World!";
+echo "Original string: " . $string . "<br>";
+
+echo "String in UPPERCASE: " . transformString($string, 'UC') . "<br>";
+echo "String in lowercase: " . transformString($string, 'LC') . "<br>";
+echo "String with First Character in Uppercase: " . transformString($string, 'FCUC') . "<br>";
+?>
